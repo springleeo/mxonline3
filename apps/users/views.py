76d1327321e095f9ddcf3fcf49d66e9142df7341
springleeo <springleeo@163.com>
2020-06-11 11:16:14 +0800

    1.登录换成LoginView，写了get和post方法 2.通过form进行表单验证并把error传到前台

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from .models import UserProfile
 from django.db.models import Q
+from django.views.generic.base import View
+from .forms import LoginForm
 
 
 # 实现用户名邮箱均可登录
@@ -21,31 +23,34 @@ class CustomBackend(ModelBackend):
 			return None
 
 
-# 当我们配置url被这个view处理时，自动传入request对象.
-def user_login(request):
-	# 前端向后端发送的请求方式: get 或post
-
-	# 登录提交表单为post
-	if request.method == 'POST':
-		# 取不到时为空，username，password为前端页面name值
-		user_name = request.POST.get('username', '')
-		pass_word = request.POST.get('password', '')
-		# 成功返回user对象,失败返回null
-		user = authenticate(username=user_name, password=pass_word)
-		# 如果不是null说明验证成功
-		if user is not None:
-			# login_in 两参数：request, user
-			# 实际是对request写了一部分东西进去，然后在render的时候：
-			# request是要render回去的。这些信息也就随着返回浏览器。完成登录
-			login(request, user)
-			# 跳转到首页 user request会被带回到首页
-			return render(request, "index.html")
-		# 没有成功说明里面的值是None，并再次跳转回主页面
-		else:
-			return render(request, "login.html", {'msg':'用户名或密码错误'})
-
-
-	elif request.method == 'GET':
+class LoginView(View):
+	# 直接调用get方法免去判断
+	def get(self, request):
 		# render就是渲染html返回用户
 		# render三变量: request 模板名称 一个字典写明传给前端的值
-		return render(request, 'login.html', {})
+		return render(request, "login.html", {})
+
+	def post(self, request):
+		login_form = LoginForm(request.POST)
+		# is_valid判断我们字段是否有错执行我们原有逻辑，验证失败跳回login页面
+		if login_form.is_valid():
+			# 取不到时为空，username，password为前端页面name值
+			user_name = request.POST.get('username', '')
+			pass_word = request.POST.get('password', '')
+			# 成功返回user对象,失败返回null
+			user = authenticate(username=user_name, password=pass_word)
+			# 如果不是null说明验证成功
+			if user is not None:
+				# login_in 两参数：request, user
+				# 实际是对request写了一部分东西进去，然后在render的时候：
+				# request是要render回去的。这些信息也就随着返回浏览器。完成登录
+				login(request, user)
+				# 跳转到首页 user request会被带回到首页
+				return render(request, "index.html")
+			# 没有成功说明里面的值是None，并再次跳转回主页面
+			else:
+				return render(request, "login.html", {'msg': '用户名或密码错误'})
+		# 验证不成功跳回登录页面
+		# 没有成功说明里面的值是None，并再次跳转回主页面
+		else:
+			return render(request, "login.html", {"login_form": login_form})
--- mxonline3/urls.py
+++ mxonline3/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 import xadmin
 from django.views.generic import TemplateView
-from users.views import user_login
+from users.views import LoginView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     # path('login/', TemplateView.as_view(template_name='login.html'), name='login')
-    path('login/', user_login, name='login')
+    path('login/', LoginView.as_view(), name='login')
 ]
